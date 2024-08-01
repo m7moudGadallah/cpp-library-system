7e47255d581e1791d16825a81e82e7df5c084a88
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-08-01 23:23:15 +0300

    feat(global): add global header file

---
+++ include/global.hpp
@@ -0,0 +1,9 @@
+#ifndef GLOBAL_HPP_
+#define GLOBAL_HPP_
+
+#define FILE_DELIMITER ",,,"
+#define BOOKS_FILE_PATH "data/books.txt"
+#define USERS_FILE_PATH "data/users.txt"
+#define BORROWED_BOOKS_FILE_PATH "data/borrowed-books.txt"
+
+#endif // GLOBAL_HPP_
