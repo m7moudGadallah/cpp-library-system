bb22213204102da74b7e304c22d3ec535ec47b77
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-08-02 01:27:03 +0300

    feat(user-repo): add user repository class header file

---
+++ include/user-repository.hpp
@@ -0,0 +1,28 @@
+#ifndef USE_REPOSITORY_HPP_
+#define USER_REPOSITORY_HPP_
+
+#include "user.hpp"
+
+#include <string>
+#include <vector>
+
+class UserRepository {
+  private:
+    std::vector<User> users;
+    const std::vector<std::string> fileHeaderTokens = {"ID", "NAME"};
+
+  public:
+    UserRepository();
+    ~UserRepository();
+    void addUser(const User &user);
+    User getUserById(int id) const;
+    std::vector<User> getUsers() const;
+    bool isUserExist(std::string name);
+
+  private:
+    void loadUsers();
+    void saveUsers();
+    int findUserIndexById(int id) const;
+};
+
+#endif // USER_REPOSITORY_HPP_
