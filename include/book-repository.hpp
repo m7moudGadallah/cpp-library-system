0bf1bc882e12861ac0ea29d6d23eac69d8058265
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-08-01 23:44:12 +0300

    feat(book-repo): add book repository class header file

---
+++ include/book-repository.hpp
@@ -0,0 +1,29 @@
+#ifndef BOOK_REPOSITORY_HPP_
+#define BOOK_REPOSITORY_HPP_
+
+#include "book.hpp"
+#include "file-handler.hpp"
+
+class BookRepository {
+  private:
+    std::vector<Book> books;
+    const std::vector<std::string> fileHeaderTokens = {"ID", "NAME", "QUANTITY",
+                                                       "BORROWED"};
+
+  public:
+    BookRepository();
+    ~BookRepository();
+    void addBook(const Book &book);
+    Book borrowBook(int id);
+    Book returnBook(int id);
+    std::vector<Book> getBooks() const;
+    std::vector<Book> getBooksByNamePrefix(std::string namePrefix) const;
+    bool isBookExist(std::string name) const;
+
+  private:
+    void loadBooks();
+    void saveBooks();
+    int findBookIndexById(int id) const;
+};
+
+#endif // BOOK_REPOSITORY_HPP_
