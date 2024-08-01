1cf72cbdb8bb38138884a9609767cb1c70667e41
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-08-01 17:59:50 +0300

    feat(library-system): add library system .cpp file

---
+++ src/library-system.cpp
@@ -0,0 +1,9 @@
+#include "../include/library-system.hpp"
+
+#include <iostream>
+
+void LibrarySystem::run() {
+    // TODO: implement run method
+    std::cerr << "System run not implemented yet!\n";
+    exit(EXIT_FAILURE);
+}
